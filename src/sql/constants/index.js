c3cdeb1b669326e41ae8f864b586618d435d1310
Heorhii <jorhesancho@gmail.com>
2022-08-09 18:22:44 +0300

    setup package

--- src/sql/constants/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.constants = void 0;
-var mysql_1 = require("./mysql");
-exports.constants = mysql_1.MySQLConstants;
-//# sourceMappingURL=index.js.map
